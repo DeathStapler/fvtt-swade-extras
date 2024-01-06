6b95508c27f4fc793fce2d7d461806d572f85356
DeathStapler <ray@hodel.com>
2024-01-06 18:24:21 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 
+
+Hooks.on(`ready`, () => {
+    console.log("SWADE Extras | Ready");
+
+    game.swadeextras = {};
+    game.swadeextras.path = "modules/swade-extras";
+
+    const  templatePaths = [
+        game.swadeextras.path + "/templates/dramatic-task.hbs",
+    ]
+
+    loadTemplates(templatePaths).then(() => {
+        console.log("SWADE Extras templates preloaded");
+      });
+
+});
+
+
+Hooks.on("renderChatMessage", (message, html) => {
+
+});
