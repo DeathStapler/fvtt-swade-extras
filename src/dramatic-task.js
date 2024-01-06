4314414595bd7609ddfdbce0a932ab5e1a79456a
DeathStapler <ray@hodel.com>
2024-01-06 18:23:22 -0500

    Update dramatic-task.js

--- src/dramatic-task.js
+++ src/dramatic-task.js
@@ -1,1 +1,12 @@
+import { SwadeExtrasCard } from "./swade-extras-card";
 
+export class DramaticTaskCard extends SwadeExtrasCard {
+    constructor(message) {
+        super(message);
+        this.type = 
+    }
+
+    async save() {
+        super();
+    }
+}
